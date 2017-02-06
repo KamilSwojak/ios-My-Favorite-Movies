674c8d450fd121ee1b0da721610b34b4ec336986
KamilSwojak <kamil.swojak@gmail.com>
2017-02-06 20:55:53 +0100

    Add SubtitleCollectionViewCell

---
+++ MyFavoriteMovies/App/View/CollectionViewCell/SubtitleCollectionViewCell/SubtitleCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedCrewCollectionViewCell.swift
+//  MyFavoriteMovies
+//
+//  Created by Kamil Swojak on 24/01/2017.
+//  Copyright © 2017 Kamil Swojak. All rights reserved.
+//
+
+import UIKit
+
+class SubtitleCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel! 
+}
