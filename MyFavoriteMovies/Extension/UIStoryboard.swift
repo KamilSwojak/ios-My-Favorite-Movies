591e48a5fee37f1f7d0024a0d25b9c1be19f5e65
KamilSwojak <kamil.swojak@gmail.com>
2017-02-06 21:47:11 +0100

    Add static initializers for storyboard files

--- MyFavoriteMovies/Extension/UIStoryboard.swift
+++ MyFavoriteMovies/Extension/UIStoryboard.swift
@@ -10,4 +10,27 @@ import UIKit
 
 extension UIStoryboard{
     
+    static var navigationViewController: UINavigationController{
+        return UIStoryboard(name: "Navigation", bundle: nil).instantiateInitialViewController() as! UINavigationController
+    }
+    
+    static var sidePanel: SidePanelViewController{
+        return UIStoryboard(name: "SidePanel", bundle: nil).instantiateInitialViewController() as! SidePanelViewController
+    }
+    
+    static var movieCollections: MovieCollectionsViewController {
+        return UIStoryboard(name: "MovieCollections", bundle: nil).instantiateInitialViewController() as! MovieCollectionsViewController
+    }
+    
+    static func movieDetails(movie: Movie) -> MovieDetailsViewController{
+        let s = UIStoryboard(name: "MovieDetails", bundle: nil)
+        let vc = s.instantiateViewController(withIdentifier: "MovieDetailsViewController") as! MovieDetailsViewController
+        vc.movie = movie
+        return vc
+    }
+    
+    static var tmdbLogin: UIViewController {
+        let s = UIStoryboard(name: "Login", bundle: nil)
+        return s.instantiateInitialViewController()!
+    }
 }
