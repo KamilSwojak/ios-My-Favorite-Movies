c7b28f1e13cfc54f468dbfd9c8fc0d898a5ca32d
KamilSwojak <kamil.swojak@gmail.com>
2017-02-06 20:55:00 +0100

    Add MovieCollectionViewCell

---
+++ MyFavoriteMovies/App/View/CollectionViewCell/MovieCollectionViewCell/MovieCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MovieCellCollectionViewswift
+//  MyFavoriteMovies
+//
+//  Created by Kamil Swojak on 23/01/2017.
+//  Copyright © 2017 Kamil Swojak. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var poster: TmdbImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var rating: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    @IBOutlet weak var star: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = UIColor.white
+        
+        layer.masksToBounds = false
+        layer.cornerRadius = 3.0
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+        layer.shadowColor = Color.secondary.cgColor
+        layer.shadowOpacity = 0.6
+        
+        layer.borderColor = Color.secondary.cgColor
+    }
+    
+}
