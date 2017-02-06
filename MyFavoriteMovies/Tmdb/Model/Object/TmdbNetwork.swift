6821b00fa32950ef2f0d26a365f34d596399bc50
KamilSwojak <kamil.swojak@gmail.com>
2017-02-06 07:13:24 +0100

    Rename to TmdbNetwork

---
+++ MyFavoriteMovies/Tmdb/Model/Object/TmdbNetwork.swift
@@ -0,0 +1,21 @@
+//
+//  TmdbCompany.swift
+//  MyFavoriteMovies
+//
+//  Created by Kamil Swojak on 13/01/2017.
+//  Copyright © 2017 Kamil Swojak. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class TmdbNetwork: TmdbResponse{
+    
+    var id: Int?
+    var name: String?
+    
+    override func mapping(map: Map) {
+        id <- map["id"]
+        name <- map["name"]
+    }
+}
