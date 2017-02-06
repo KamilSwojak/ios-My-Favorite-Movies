7a246e89644312c9ea8f9a493ffd573e7e2a31ff
KamilSwojak <kamil.swojak@gmail.com>
2017-02-06 20:55:12 +0100

    Add ProfileCollectionViewCell

---
+++ MyFavoriteMovies/App/View/CollectionViewCell/ProfileListCollectionViewCell/ProfileCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TopBilledCastCellCollectionViewCell.swift
+//  MyFavoriteMovies
+//
+//  Created by Kamil Swojak on 24/01/2017.
+//  Copyright © 2017 Kamil Swojak. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var picture: TmdbImageView!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        picture.layer.masksToBounds = true
+        picture.layer.cornerRadius = picture.frame.width / 2
+        picture.layer.borderColor = Color.secondary.cgColor
+        picture.layer.borderWidth = 2.0
+    }
+}
