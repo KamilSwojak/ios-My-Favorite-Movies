f24a91394618ceca9401a71d5deab55e65ab4c8a
KamilSwojak <kamil.swojak@gmail.com>
2017-02-06 21:47:31 +0100

    Add colours used in app

---
+++ MyFavoriteMovies/Extension/UIColor.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor.swift
+//  MyFavoriteMovies
+//
+//  Created by Kamil Swojak on 06/02/2017.
+//  Copyright © 2017 Kamil Swojak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+
+    /// #colorLiteral(red: 0, green: 0.841566205, blue: 0.5404788852, alpha: 1)
+    static let primary = UIColor(red: 0.003, green: 0.841, blue: 0.5404, alpha: 1.0) //
+    
+    /// #colorLiteral(red: 0.02011371963, green: 0.1463510096, blue: 0.188203007, alpha: 1)
+    static let secondary = UIColor(red: 0.02, green: 0.146, blue: 0.188, alpha: 1.0)
+
+}
