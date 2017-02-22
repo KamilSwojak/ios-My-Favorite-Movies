409b306b124abdf90f4dce6adf34e2d1a44074df
KamilSwojak <kamil.swojak@gmail.com>
2017-02-22 17:22:18 +0100

    Add view taps

--- MyFavoriteMovies/App/MovieDetails/MovieDetailContract.swift
+++ MyFavoriteMovies/App/MovieDetails/MovieDetailContract.swift
@@ -11,14 +11,36 @@ import RxSwift
 
 struct MovieDetailsViewOutput: ViewModelInputType{
     var movie: Movie
+    var playTrailerTaps: Observable<Void>
+    var userListsTaps: Observable<Void>
+    var favoriteTaps: Observable<Void>
+    var bookmarkTaps: Observable<Void>
+    var rateTaps: Observable<Void>
+    var genreTap: Observable<String>
+    var keywordTap: Observable<String>
+    var homepageLinkTap: Observable<Void>
+    var crewTap: Observable<(indexPath: IndexPath, crew: MovieCrew)>
+    var castTap: Observable<(indexPath: IndexPath, cast: MovieCast)>
 }
 
 struct MovieDetailsViewModelOutput: ViewModelOutputType{
     var movie: Observable<Movie>
-    var crew: ReactiveList<MovieCrew>
-    var cast: ReactiveList<MovieCast>
-    var keywords: ReactiveList<Keyword>
-    var genres: ReactiveList<Genre>
-    var trailers: Observable<[URL]>
+    var crew: Observable<[MovieCrew]>
+    var cast: Observable<[MovieCast]>
+    var keywords: Observable<[Keyword]>
+    var genres: Observable<[Genre]>
     var trailerButtonVisibility: Observable<Bool>
+    var event: Observable<MovieDetailsEvent>
+}
+
+enum MovieDetailsEvent {
+    case Trailer(_: URL)
+    case AddToLists
+    case Favorite
+    case Bookmark
+    case Rate
+    case SelectedTag(_: String)
+    case Open(_: URL)
+    case SelectedCrew(_: MovieCrew)
+    case SelectedCast(_: MovieCast)
 }
