97b4d528d0c5a90d9674eb53060b1e932d11fd5b
KamilSwojak <kamil.swojak@gmail.com>
2017-02-22 14:58:39 +0100

    Add enum with date format types

---
+++ MyFavoriteMovies/Extension/DateFormatter.swift
@@ -0,0 +1,15 @@
+//
+//  DateFormatter.swift
+//  MyFavoriteMovies
+//
+//  Created by Kamil Swojak on 22/02/2017.
+//  Copyright © 2017 Kamil Swojak. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+    enum Format : String {
+        case yyyy_MM_dd = "yyyy-MM-dd"
+    }
+}
