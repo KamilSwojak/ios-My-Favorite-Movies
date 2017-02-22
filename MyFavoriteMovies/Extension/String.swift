4c7260aef17e93d20f00946bc60d9a38e389b8d7
KamilSwojak <kamil.swojak@gmail.com>
2017-02-22 14:53:05 +0100

    Add helpers to format string as date

--- MyFavoriteMovies/Extension/String.swift
+++ MyFavoriteMovies/Extension/String.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
+//MARK: Dimensions
 extension String {
+    
     func heightWithFixedWidth(width: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
         let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
-        
+
         return boundingBox.height
+    }
+}
+
+//MARK: Format
+enum StringFormatType {
+    
+    case Date(format: DateFormatter.Format, style: DateFormatter.Style)
+    
+}
+
+extension String {
+    
+    func format(as format: StringFormatType) -> String? {
+        switch format {
+        case .Date(format: let format, let style):
+
+            let formatter = DateFormatter()
+            formatter.dateFormat = format.rawValue
+            
+            guard let date = formatter.date(from: self) else { return nil }
+            
+            formatter.dateStyle = style
+            
+            return formatter.string(from: date)
+        }
+    }
+}
+
+
+//MARK: Substring
+extension String {
+    
+    func substring(to: Int) -> String {
+        return (self as NSString).substring(to: 4)
     }
 }
