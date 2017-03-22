57205a4ed60c31aaade7de9041270dab06ff23d2
KamilSwojak <kamil.swojak@gmail.com>
2017-03-22 17:13:40 +0100

    fix imports

--- MyFavoriteMovies/App/MovieDetails/MovieDetailViewModel.swift
+++ MyFavoriteMovies/App/MovieDetails/MovieDetailViewModel.swift
@@ -9,6 +9,7 @@
 import UIKit
 import RxSwift
 import TmdbApi
+import KSReactiveXExtensions
 
 final class MovieDetailsViewModel: BaseViewModel<MovieDetailsViewOutput, MovieDetailsViewModelOutput> {
     
