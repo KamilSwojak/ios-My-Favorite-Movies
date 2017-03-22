ef0f41cf97b4be4cd77dac423a6801221832da22
KamilSwojak <kamil.swojak@gmail.com>
2017-03-22 17:14:59 +0100

    Remove deleted cells

--- MyFavoriteMovies/Extension/UITableViewCell.swift
+++ MyFavoriteMovies/Extension/UITableViewCell.swift
@@ -9,29 +9,9 @@
 import UIKit
 
 extension UITableViewCell{
-
-    static var movie: MovieTableViewCell {
-        return Bundle.main.loadNibNamed("MovieTableViewCell", owner: self, options: nil)?.first as! MovieTableViewCell
-    }
-    
-    static var movieData: MovieDataTableViewCell {
-        return Bundle.main.loadNibNamed("MovieDataTableViewCell", owner: self, options: nil)?.first as! MovieDataTableViewCell
-    }
     
     static var verticalMovieList: VerticalMovieListTableViewCell {
         return Bundle.main.loadNibNamed("VerticalMovieListTableViewCell", owner: self, options: nil)?.first as! VerticalMovieListTableViewCell
-    }
-    
-    static var label: LabelTableViewCell {
-        return Bundle.main.loadNibNamed("LabelTableViewCell", owner: self, options: nil)?.first as! LabelTableViewCell
-    }
-    
-    static var subtitle: SubtitleTableViewCell {
-        return Bundle.main.loadNibNamed("SubtitleTableViewCell", owner: self, options: nil)?.first as! SubtitleTableViewCell
-    }
-    
-    static var subtitleList: SubtitleListTableViewCell {
-        return Bundle.main.loadNibNamed("SubtitleListTableViewCell", owner: self, options: nil)?.first as! SubtitleListTableViewCell
     }
     
     static var profileList: ProfileListTableViewCell {
